583e717d8235ecfa78c217010d40cc3c96956172
Thomas Seberechts <thomas@itprosolutions.be>
2016-04-18 11:19:54 +0200

    initial commit

--- build/args.js
+++
@@ -1,8 +0,0 @@
-var yargs = require('yargs');
-
-var argv = yargs.argv,
-    bump = (argv.bump).toLowerCase();
-
-module.exports = {
-  bump: bump
-};
--- build/tasks/lint.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
-var stylish = require('jshint-stylish');
-
-gulp.task('lint', function() {
-  return gulp.src('../../src')
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish));
-});
